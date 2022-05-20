cc469b81972c32b0c8030946d229abd01d8f1afe
Akudbilla Daniel <akuddev@gmail.com>
2022-05-20 17:05:56 +0000

    added register user functionality

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the users.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new user.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("users.register");
+    }
+
+    /**
+     * Store a newly created user in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //Validate user data
+        $formData= $request->validate([
+            'name'=>'required|min:2',
+            'email'=>'required|email|unique:users',
+            'password'=>'required|confirmed|min:6'
+        ]);
+
+        //hash password
+        $formData['password']=bcrypt($formData['password']);
+
+        //create new user with data
+        $user=User::create($formData);
+
+        //Login user
+        auth()->login($user);
+
+        return redirect('/')->with('message','User created succesfully');
+    }
+
+    /**
+     * Display the specified user.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified user.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified user in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified user from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -38,3 +39,8 @@ Route::delete("/listings/{listing}/",[ListingController::class,'destroy']);
 Route::get("/listings/{listing}/",[ListingController::class,'show']);
 
 
+//Show register/create form
+Route::get('/register',[UserController::class,'create']);
+
+//create new users
+Route::post('/users',[UserController::class,'store']);
