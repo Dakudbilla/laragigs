be850d9e87849a3ff54060c5cacb4fbeb88a6fd6
Akudbilla Daniel <akuddev@gmail.com>
2022-05-21 21:46:37 +0000

    link location to google maps

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -2,18 +2,24 @@
     <x-card>
 
         <div class="flex">
+            <a href="./listings/{{ $listing->id }}">
             <img class="hidden w-48 mr-6 md:block"
                 src="{{ $listing->logo?asset('storage/'.$listing->logo) :asset('images/no-image.png') }}"
                 alt="" />
+            </a>
             <div>
-                <h3 class="text-2xl">
+                <h3 class="text-2xl mb-3">
                     <a href="./listings/{{ $listing->id }}">{{ $listing->title }}</a>
                 </h3>
                 <div class="text-xl font-bold mb-4">{{ $listing->company }}</div>
                 <x-listing-tags :tagsCsv="$listing->tags" />
+            <a target="_blank" href="https://www.google.com/maps/search/?api=1&query={{$listing->location}}">
+
                 <div class="text-lg mt-4">
                     <i class="fa-solid fa-location-dot"></i> {{ $listing->location }}
+
                 </div>
+            </a>
             </div>
         </div>
     </x-card>
